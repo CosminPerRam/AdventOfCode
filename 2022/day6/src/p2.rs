8a19c9cbb93014f9498b9a39e561bab27427f24d
CosminPerRam <cosmin.p@live.com>
2022-12-07 15:12:29 +0200

    2022 Day 6

---
+++ 2022/day6/src/p2.rs
@@ -0,0 +1,48 @@
+use std::collections::{HashSet, VecDeque};
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
+const QUEUE_UNIQUE_LENGTH: usize = 14;
+// Part 2 is just part 1 with 14-length check
+
+fn has_repeated_entries(queue: &VecDeque<char>) -> bool {
+    let mut uniq = HashSet::new();
+    queue.into_iter().all(move |x| uniq.insert(x))
+}
+
+fn add_character(queue: &mut VecDeque<char>, character: char) {
+    if queue.len() == QUEUE_UNIQUE_LENGTH {
+        queue.pop_front();
+    }
+
+    queue.push_back(character);
+}
+
+fn is_unique(queue: &VecDeque<char>) -> bool {
+    if queue.len() != QUEUE_UNIQUE_LENGTH {
+        false
+    }
+    else {
+        has_repeated_entries(queue)
+    }
+}
+
+pub fn run() {
+    let file = File::open("day6.txt").unwrap();
+    let reader = BufReader::new(file);
+
+    let mut line_result = reader.lines();
+    let line_binding = line_result.next().unwrap().unwrap();
+    let line = line_binding.chars();
+
+    let mut queue: VecDeque<char> = VecDeque::with_capacity(4);
+
+    for (index, character) in line.enumerate() {
+        add_character(&mut queue, character);
+
+        if is_unique(&queue) {
+            println!("{}", index + 1);
+            return;
+        }
+    }
+}
